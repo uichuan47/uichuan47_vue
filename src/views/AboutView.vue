88993bee97bbcac15fc760d31bd05bc389d51d39
kento <uichuan_47@163.com>
2024-03-14 17:14:55 +0800

    导航栏以及axios引入

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,41 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <table border="1">
+    <thead>
+    <tr>
+      <th>ID</th>
+      <th>标题</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="item in data_list">
+      <td>{{ item.id }}</td>
+      <td>{{ item.name }}</td>
+    </tr>
+    </tbody>
+  </table>
 </template>
-<style>
+
+<script setup>
+import {onMounted, ref} from "vue";
+import axios from "axios";
+
+const data_list = ref([])
+onMounted(function (){
+  //使用axios发送请求
+  axios({
+    method: "get",
+    url: "https://api.luffycity.com/api/v1/course/category/actual/?courseType=actual",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(function (res) {
+    data_list.value = res.data.data
+
+  }).catch(function (error) {
+    console.log("请求失败", error);
+  })
+})
+</script>
+
+<style scoped>
 </style>
